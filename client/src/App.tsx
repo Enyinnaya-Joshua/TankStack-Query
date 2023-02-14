21bedab3cb08302a9d64889b521beaa08560629e
gideonekekeke <Gideonekeke64@gmail.com>
2023-02-14 15:17:21 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AllRoute from "./Components/AllRoute";
+
+function App() {
+	return (
+		<div className='App'>
+			<AllRoute />
+		</div>
+	);
+}
+
+export default App;
